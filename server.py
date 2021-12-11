0d0e0c5ce74e64f4a794f8447746f91f665b1134
Dallas Clarke <65244012+dallasclarke@users.noreply.github.com>
2021-12-10 17:27:30 -0800

    Cleaned up some code in server.py & fixed improper numbering for each page.

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 def my_home():
 	return render_template("index.html")
 
-@app.route("/blog")
-def blog():
-	return "These are my thoughts on blogs!"
+@app.route("/<string:page_name>")
+def html_page(page_name):
+	return render_template(page_name)
+
