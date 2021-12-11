41e2b780aae2a18eb5a3680bcce55dfb937b45f9
Dallas Clarke <65244012+dallasclarke@users.noreply.github.com>
2021-12-10 18:29:55 -0800

    Created thankyou.html and am able to submit form and have it redirect to thankyou.html.

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,4 +11,9 @@ def html_page(page_name):
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
-    return "form submitted!!!"
+    if request.method == "POST":
+        data = request.form.to_dict()
+        print(data)
+        return redirect("/thankyou.html")
+    else:
+        return "Something went wrong"
